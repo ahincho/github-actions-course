62af735e24e17b68056076049da9368bb7f12787
ahincho <ahincho@unsa.edu.pe>
2025-12-07 17:35:53 -0500

    Feature: Update Ping Logic

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,32 @@
+import os
+import requests
+import time
+
+def ping(url, max_free_trials=10, delay=5):
+  trials = 0
+  while trials < max_free_trials:
+    try:
+      response = requests.get(url)
+      if response.status_code == 200:
+        print(f"Website {url} is reachable.")
+        return True
+    except requests.RequestException as re:
+      print(f"Attempt {trials + 1} failed: {re}")
+      time.sleep(delay)
+    except requests.exceptions.MissingSchema as ms:
+      print(f"Invalid URL schema: {ms}")
+      return False
+    trials += 1
+  return False
+
+def run():
+  url = os.getenv("INPUT_URL", "http://example.com")
+  max_free_trials = int(os.getenv("INPUT_MAX-FREE-TRIALS", "10"))
+  delay = int(os.getenv("INPUT_DELAY", "5"))
+  is_reachable = ping(url, max_free_trials, delay)
+  if not is_reachable:
+    raise Exception(f"Website {url} is not reachable after {max_free_trials} attempts.")
+  print(f"Website {url} is reachable after {max_free_trials} attempts.")
+
 if __name__ == "__main__":
-  print("This is a Docker Ping URL action.")
+  run()
